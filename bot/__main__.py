fa3c8181922a8abc4654b40038101642d2b426cc
Justin <119710965+justinlime@users.noreply.github.com>
2023-02-14 00:23:05 -0600

    Create README.org

--- bot/__main__.py
+++
@@ -1,7 +0,0 @@
-import discord
-import os
-from discord.ui import Select, View, Button
-from discord.ext import commands
-
-intents = discord.Intents.all()
-bot = commands.Bot(debug_guilds=[guild_id], intents=intents)
