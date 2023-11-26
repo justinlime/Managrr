97df51f385964b36c06c21e9697873a8a49f85a7
justinlime <justinlime1999@gmail.com>
2023-11-25 22:14:50 -0600

    README and flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Managrr";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "x86_64-darwin"
+          "aarch64-linux"
+        ] (system:
+          function (import nixpkgs {
+            inherit system;
+          }));
+    in {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_20  
+          ];
+          shellHook = ''
+            clear
+            echo "DevShell Activated"
+          '';
+        };
+      });
+    };
+}
